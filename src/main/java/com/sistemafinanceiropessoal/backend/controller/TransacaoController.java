fffe6a591392fafce568f585ec2d486ff2bab748
brendaevilly <brendaevilly014@gmail.com>
2025-06-07 22:17:54 -0300

    Configurando banco de dados mysql.

--- src/main/java/com/sistemafinanceiropessoal/backend/controller/TransacaoController.java
+++ src/main/java/com/sistemafinanceiropessoal/backend/controller/TransacaoController.java
@@ -2,16 +2,14 @@ package com.sistemafinanceiropessoal.backend.controller;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.sistemafinanceiropessoal.backend.model.Transacao;
 import com.sistemafinanceiropessoal.backend.service.TransacaoService;
